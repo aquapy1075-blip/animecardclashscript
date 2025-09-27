cde3fe4412fd59fe07213877e58f033bb5c3048a
aquapy1075-blip <aquapy1075@gmail.com>
2025-09-27 13:32:56 +0700

    Update bossglobal.lua

--- bossglobal.lua
+++ bossglobal.lua
@@ -1,3 +1,11 @@
+-- Anti AFK
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+    vu:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+    task.wait(1)
+    vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+end)
+
 -- Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
