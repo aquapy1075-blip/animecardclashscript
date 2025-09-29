88dd7df4d64228d18da1a53f862e01853a1fbdfd
aquapy1075-blip <aquapy1075@gmail.com>
2025-09-29 21:15:22 +0700

    Update aquahub.lua (them antiafk)

--- aquahub.lua
+++ aquahub.lua
@@ -1,3 +1,13 @@
+
+-- Anti AFK
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+    vu:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+    task.wait(1)
+    vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+end)
+
+
 -- Services
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
