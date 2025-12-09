a770bcf57284822630be8c2dc9824ba5e5064a61
aquapy1075-blip <aquapy1075@gmail.com>
2025-12-09 11:52:06 +0700

    Update WindUI loading method to use latest release

--- animecardclash.lua
+++ animecardclash.lua
@@ -1,7 +1,4 @@
-
-    local Version = "1.6.61"
-local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/download/" .. Version .. "/main.lua"))()
-
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
 	-- Anti AFK
 	local vu = game:GetService("VirtualUser")
